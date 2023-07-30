7587b005b9202bb7464ad9161e201fbe6d379796
masatoomori <masatoomori@users.noreply.github.com>
2023-07-30 15:14:39 +0900

    Update environment.py

--- notebooks/environment.py
+++ notebooks/environment.py
@@ -1,5 +1,5 @@
 import numpy as np
-import gym
+import gymnasium as gym
 from kaggle_environments import make, evaluate
 
 # https://github.com/Kaggle/kaggle-environments/tree/master/kaggle_environments/envs/connectx
